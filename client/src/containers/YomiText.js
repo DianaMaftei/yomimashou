bb5383d2422b4ecb5bc1b69b2cdfffbad01cf2c7
Diana Maftei <diana.maftei@gmail.com>
2018-01-02 14:23:12 +0200

    extract text parsing into util script

--- client/src/containers/YomiText.js
+++ client/src/containers/YomiText.js
@@ -6,6 +6,7 @@ import {popUpSetVisibility} from '../actions/index';
 import RikaiDict from "../components/rikai/RikaiDictionary";
 import Rikai from "../components/rikai/Rikai";
 import RikaiPopUp from '../components/rikai/Rikai-pop-up';
+import getTextFromRange from "../util/textParser";
 
 const mapStateToProps = (state) => ({
     text: state.yomi.text,
@@ -30,16 +31,6 @@ class YomiText extends React.Component {
     constructor(props) {
         super(props);
 
-        // XPath expression which evaluates to text nodes
-        // tells rikaiR which text to translate
-        // expression to get all text nodes that are not in (RP or RT) elements
-        this.textNodeExpr = 'descendant-or-self::text()[not(parent::rp) and not(ancestor::rt)]';
-
-        // XPath expression which evaluates to a boolean. If it evaluates to true
-        // then rikaiR will not start looking for text in this text node
-        // ignore text in RT elements
-        this.startElementExpr = 'boolean(parent::rp or ancestor::rt)';
-
         this.lastPos = {x: null, y: null};
         this.lastTarget = null;
         this.defaultDict = 2;
@@ -62,51 +53,6 @@ class YomiText extends React.Component {
 
         // Hack because ro was coming out always 0 for some reason.
         this.lastRo = 0;
-
-        this.inlineNames = {
-            // text node
-            '#text': true,
-
-            // font style
-            'FONT': true,
-            'TT': true,
-            'I': true,
-            'B': true,
-            'BIG': true,
-            'SMALL': true,
-            //deprecated
-            'STRIKE': true,
-            'S': true,
-            'U': true,
-
-            // phrase
-            'EM': true,
-            'STRONG': true,
-            'DFN': true,
-            'CODE': true,
-            'SAMP': true,
-            'KBD': true,
-            'VAR': true,
-            'CITE': true,
-            'ABBR': true,
-            'ACRONYM': true,
-
-            // special, not included IMG, OBJECT, BR, SCRIPT, MAP, BDO
-            'A': true,
-            'Q': true,
-            'SUB': true,
-            'SUP': true,
-            'SPAN': true,
-            'WBR': true,
-
-            // ruby
-            'RUBY': true,
-            'RBC': true,
-            'RTC': true,
-            'RB': true,
-            'RT': true,
-            'RP': true
-        };
 
         this.onMouseMove = this.onMouseMove.bind(this);
         this.onMouseClick = this.onMouseClick.bind(this);
@@ -211,21 +157,6 @@ class YomiText extends React.Component {
         }
     }
 
-    isInline(node) {
-        return this.inlineNames.hasOwnProperty(node.nodeName) ||
-            // only check styles for elements
-            // comments do not have getComputedStyle method
-            (document.nodeType === Node.ELEMENT_NODE &&
-                (document.defaultView.getComputedStyle(node, null).getPropertyValue('display') === 'inline' ||
-                document.defaultView.getComputedStyle(node, null).getPropertyValue('display') === 'inline-block')
-            );
-    }
-
-    getFirstTextChild(node) {
-        return document.evaluate('descendant::text()[not(parent::rp) and not(ancestor::rt)]',
-            node, null, XPathResult.ANY_TYPE, null).iterateNext();
-    }
-
     clearHi() {
         let tdata = window.rikaiR;
         if ((!tdata) || (!tdata.prevSelView)) return;
@@ -263,84 +194,6 @@ class YomiText extends React.Component {
         tdata.kanjiChar = null;
         tdata.selText = null;
     }
-
-    // Gets text from a node
-    // returns a string
-    // node: a node
-    // selEnd: the selection end object will be changed as a side effect
-    // maxLength: the maximum length of returned string
-    // xpathExpr: an XPath expression, which evaluates to text nodes, will be evaluated
-    // relative to "node" argument
-    getInlineText(node, selEndList, maxLength, xpathExpr) {
-        let text = '';
-        let endIndex;
-
-        if (node.nodeName === '#text') {
-            endIndex = Math.min(maxLength, node.data.length);
-            selEndList.push({node: node, offset: endIndex});
-            return node.data.substring(0, endIndex);
-        }
-
-        let result = xpathExpr.evaluate(node, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
-
-        // eslint-disable-next-line
-        while ((text.length < maxLength) && (node = result.iterateNext())) {
-            endIndex = Math.min(node.data.length, maxLength - text.length);
-            text += node.data.substring(0, endIndex);
-            selEndList.push({node: node, offset: endIndex});
-        }
-
-        return text;
-    };
-
-    // given a node which must not be null,
-    // returns either the next sibling or next sibling of the father or
-    // next sibling of the fathers father and so on or null
-    getNext(node) {
-        let nextNode;
-
-        nextNode = node.nextSibling;
-
-        if (nextNode !== null)
-            return nextNode;
-
-        nextNode = node.parentNode;
-
-        if ((nextNode !== null) && this.isInline(nextNode))
-            return this.getNext(nextNode);
-
-        return null;
-    };
-
-    getTextFromRange(rangeParent, offset, selEndList, maxLength) {
-        let endIndex;
-        if (rangeParent.nodeName === 'TEXTAREA' || rangeParent.nodeName === 'INPUT') {
-            endIndex = Math.min(rangeParent.data.length, offset + maxLength);
-            return rangeParent.value.substring(offset, endIndex);
-        }
-
-        let text = '';
-
-        let xpathExpr = rangeParent.ownerDocument.createExpression(this.textNodeExpr, null);
-
-        if (rangeParent.ownerDocument.evaluate(this.startElementExpr, rangeParent, null, XPathResult.BOOLEAN_TYPE, null).booleanValue)
-            return '';
-
-        if (rangeParent.nodeType !== Node.TEXT_NODE)
-            return '';
-
-        endIndex = Math.min(rangeParent.data.length, offset + maxLength);
-        text += rangeParent.data.substring(offset, endIndex);
-        selEndList.push({node: rangeParent, offset: endIndex});
-
-        let nextNode = rangeParent;
-
-        // eslint-disable-next-line
-        while (((nextNode = this.getNext(nextNode)) != null) && (this.isInline(nextNode)) && (text.length < maxLength))
-            text += this.getInlineText(nextNode, selEndList, maxLength - text.length, xpathExpr);
-
-        return text;
-    };
 
     processEntry(e) {
         let tdata = window.rikaiR;
@@ -490,7 +343,7 @@ class YomiText extends React.Component {
 
         //selection end data
         let selEndList = [];
-        let text = this.getTextFromRange(rp, ro, selEndList, 13 /*maxlength*/);
+        let text = getTextFromRange(rp, ro, selEndList, 13 /*maxlength*/);
 
         this.lastSelEnd = selEndList;
         this.lastRo = ro;
@@ -501,6 +354,11 @@ class YomiText extends React.Component {
 
         return 1;
 
+    }
+
+    getFirstTextChild(node) {
+        return document.evaluate('descendant::text()[not(parent::rp) and not(ancestor::rt)]',
+            node, null, XPathResult.ANY_TYPE, null).iterateNext();
     }
 
     tryUpdatePopup(ev) {
