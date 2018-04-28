c0a929a60a2b273cf5d2f1b3c81c3bf17d2a78a7
Diana <diana.maftei@gmail.com>
2018-04-28 15:37:12 +0300

    delete old files

--- server/src/main/java/com/github/dianamaftei/yomimashou/repository/WordEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.dianamaftei.yomimashou.repository;
-
-import com.github.dianamaftei.yomimashou.model.WordEntry;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-
-@Component
-public interface WordEntryRepository extends CrudRepository<WordEntry, Long> {
-}
