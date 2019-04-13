cc2ced332a56aa2c95d3a37af5a64ba2bceedf5b
Diana <diana.maftei@gmail.com>
2019-04-13 23:45:35 +0300

    fix tests

--- frontend/src/pages/add/Add.test.js
+++ frontend/src/pages/add/Add.test.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import Add from "./Add";
-import Editor from "react-pell";
+import Text from "./Text";
+import Tags from "./Tags";
+import Series from "./Series";
+import ActionButtons from "./ActionButtons";
 
 describe("Add", () => {
     let props;
@@ -19,7 +22,9 @@ describe("Add", () => {
     beforeEach(() => {
         props = {
             text: {},
-            setText: undefined,
+            tags:[],
+            setTitle: jest.fn(),
+            setText: jest.fn(),
             history: {
                 push: jest.fn()
             },
@@ -32,17 +37,22 @@ describe("Add", () => {
         expect(wrapper().find("#add-page")).toHaveLength(1);
     });
 
-    it("should render the title field", () => {
-        expect(wrapper().find("#title-required")).toHaveLength(1);
+    it("should contain a Text component with the text title value", () => {
+        props.text.title = "some content";
+        expect(wrapper().find(Text).props().title).toBe(props.text.title);
     });
 
-    it("should render the upload file button", () => {
-        expect(wrapper().find("#outlined-button-file")).toHaveLength(1);
+    it("should contain a Tags component with the text tags value", () => {
+        props.text.tags = ["tag1", "tag2"];
+        expect(wrapper().find(Tags).props().tags).toBe(props.text.tags);
     });
 
-    it("should contain an Editor field with the text content value", () => {
-        props.text.content = "some content";
-        expect(wrapper().find(Editor).props().defaultContent).toBe(props.text.content);
+    it("should contain a Series component", () => {
+        expect(wrapper().find(Series)).toHaveLength(1);
+    });
+
+    it("should contain an ActionButtons component", () => {
+        expect(wrapper().find(ActionButtons)).toHaveLength(1);
     });
 
 });
