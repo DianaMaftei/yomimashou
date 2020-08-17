54383a3997150eeacd7a6a58f2e4999ce50c6358
Diana <diana.maftei@gmail.com>
2020-08-17 20:13:05 +0300

    Scraper to prefill app with texts - HukumusumeScraper - NhkNewsEasyScraper - basic auth for /text, as well as token - text now contains info about original source, as well as character count - images also support gif ending - cleanup: remove parts of speech conversion

--- backend/src/main/java/com/github/dianamaftei/yomimashou/security/WebSecurity.java
+++ backend/src/main/java/com/github/dianamaftei/yomimashou/security/WebSecurity.java
@@ -1,10 +1,10 @@
 package com.github.dianamaftei.yomimashou.security;
 
 import com.github.dianamaftei.yomimashou.user.ApplicationUserService;
-import java.util.Arrays;
-import java.util.Collections;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,11 +12,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 @EnableWebSecurity
+@Order(2)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     private final ApplicationUserService applicationUserService;
     private PasswordEncoder passwordEncoder;
@@ -33,14 +38,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.logout().permitAll().logoutSuccessUrl("/api/users/afterLogout")
                 .and().cors().and().csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/text").authenticated()
-            .antMatchers(HttpMethod.POST, "/api/file").authenticated()
-            .antMatchers(HttpMethod.POST, "/api/users/textStatus").authenticated()
-            .antMatchers(HttpMethod.GET, "/api/users/textStatus").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/file").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/users/textStatus").authenticated()
+                .antMatchers(HttpMethod.GET, "/api/users/textStatus").authenticated()
                 .anyRequest().permitAll()
                 .and()
-            .addFilter(new JWTAuthenticationFilter(authenticationManager(), secret))
-            .addFilter(new JWTAuthorizationFilter(authenticationManager(), secret))
+                .addFilter(new JWTAuthenticationFilter(authenticationManager(), secret))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager(), secret))
                 // this disables session creation on Spring Security
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
@@ -58,5 +62,22 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         config.setExposedHeaders(Collections.unmodifiableList(Arrays.asList("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Username")));
         source.registerCorsConfiguration("/**", config);
         return source;
+    }
+
+    @Configuration
+    @Order(1)
+    public static class BasicWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Value("${security.SECRET}")
+        public String secret;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors().and().csrf().disable().authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/api/text").authenticated()
+                    .and().httpBasic().and()
+                    .addFilterBefore(new JWTAuthenticationFilter(authenticationManager(), secret), BasicAuthenticationFilter.class)
+                    .addFilterBefore(new JWTAuthorizationFilter(authenticationManager(), secret), BasicAuthenticationFilter.class);
+        }
     }
 }
